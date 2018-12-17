03ba8273696ae8f21b1169ea7fb7125868c43b63
gunasekaranhcl <gunasekaran.r@hcl.com>
2018-12-17 15:45:08 +0530

    create app.java

---
+++ src/main/java/com/camel/hl7/rest/App.java
@@ -0,0 +1,16 @@
+package com.camel.hl7.rest;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+
+@SpringBootApplication
+public class App {
+
+  
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(App.class, args);
+    }
+
+}
